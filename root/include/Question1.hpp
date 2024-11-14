fa869ccd5633579cfe0d9c6ed7e65626c68a75d2
AidanMcGrath2024 <awm6052@psu.edu>
2024-11-14 12:54:34 -0500

    Update Question1.hpp

--- root/include/Question1.hpp
+++ root/include/Question1.hpp
@@ -1,1 +1,58 @@
-#Question 1 header files for cpp
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <memory>
+using namespace std;
+
+class Sensor {
+public:
+    Sensor() {};
+    virtual gatherData() {};
+    virtual processData() {};
+};
+
+class Radar : public Sensor
+{
+public:
+    Radar() {};
+    virtual gatherData() {
+        cout << "Gathering data from Radar Sensor." << endl;
+    }
+    virtual processData() {
+        cout << "Processing data from Radar Sensor." << endl;
+    }
+
+};
+
+class LiDAR : public Sensor
+{
+public:
+    LiDAR() {};
+    virtual gatherData() {
+        cout << "Gathering data from LiDAR Sensor." << endl;
+    }
+    virtual processData() {
+        cout << "Processing data from LiDAR Sensor." << endl;
+    }
+
+};
+
+class IMU : public Sensor
+{
+public:
+    IMU() {};
+    virtual gatherData() {
+        cout << "Gathering data from IMU Sensor." << endl;
+    }
+    virtual processData() {
+        cout << "Processing data from IMU Sensor." << endl;
+    }
+
+};
+
+class SensorFactory {
+    public:
+    static ptr createSensor(string ) {
+
+    }
+}
