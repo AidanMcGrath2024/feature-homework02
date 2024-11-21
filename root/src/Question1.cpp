1d239f520436a496101cdee1222300bd03a745bf
AidanMcGrath2024 <awm6052@psu.edu>
2024-11-21 16:25:58 -0500

    Update Question1.cpp

--- root/src/Question1.cpp
+++ root/src/Question1.cpp
@@ -1,1 +1,22 @@
-#Question 1 code
+#include "Question1.hpp"
+
+int main() {
+    AerospaceControlSystem ctrlSys; // instantiating the ctrlSys object as an AerospaceControlSystem class
+
+    Sensor* Radar = SensorFactory::createSensor("Radar");
+    if (Radar != nullptr) {
+        ctrlSys.addSensor(Radar);
+    }
+    Sensor* LiDAR = SensorFactory::createSensor("LiDAR");
+    if (LiDAR != nullptr) {
+        ctrlSys.addSensor(LiDAR);
+    }
+    Sensor* IMU = SensorFactory::createSensor("IMU");
+    if (IMU != nullptr) {
+        ctrlSys.addSensor(IMU);
+    }
+
+    ctrlSys.monitorAndAdjust();
+
+    return 0;
+}
