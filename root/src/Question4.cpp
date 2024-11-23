0956ba258203d59dce2c461f31de274362ed5ace
AidanMcGrath2024 <awm6052@psu.edu>
2024-11-23 00:59:59 -0500

    Update Question4.cpp

--- root/src/Question4.cpp
+++ root/src/Question4.cpp
@@ -1,1 +1,10 @@
-//Question 4 Source FIle
+#include "Question4.hpp"
+
+int main()
+{
+	Part1 execute1;
+	execute1.plot1(); // run plot 1 function
+	Part2 execute2;
+	execute2.plot2(); // run plot 2 function 
+	return 0;
+}
