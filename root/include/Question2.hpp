ba43cf3d65b579e819cc4475e94fe84e33406058
AidanMcGrath2024 <awm6052@psu.edu>
2024-11-22 23:03:05 -0500

    Update Question2.hpp

--- root/include/Question2.hpp
+++ root/include/Question2.hpp
@@ -1,1 +1,13 @@
-#Question2 header files for cpp
+#include "Question2.hpp"
+
+void question2() {
+     vector<thread> robots;  // container to store all the ongoing robot threads
+
+    for (int i = 0; i < numberOfRobots; ++i) {
+        robots.push_back(thread(robotTask, i));  // initiating the robot task in a new thread
+    }
+
+    for (auto& robot : robots) { // for loop to wait for all robot threads to complete their tasks
+        robot.join();  // joining each robot thread, the main "thread" waits for all robots to be done their tasks
+    }
+}
