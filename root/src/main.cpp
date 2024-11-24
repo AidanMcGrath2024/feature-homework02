44f4a9ebe487902b21f588436b555ae1405adf41
AidanMcGrath2024 <awm6052@psu.edu>
2024-11-23 21:17:50 -0500

    Update main.cpp

--- root/src/main.cpp
+++ root/src/main.cpp
@@ -1,9 +1,20 @@
-//#include "Question1.hpp"
+#include "Question1.hpp"
 #include "Question2.hpp"
-//#include "Question3.hpp"
-//#include "Question4.hpp"
+#include "Question3.hpp"
+#include "Question4.hpp"
 
 
 int main(){
+    cout << "*** Begin Question 1 ***" << endl;
+    question1();
+    cout << "*** End Question 1 ***" << endl;
+    cout << "*** Begin Question 2 ***" << endl;
     question2();
+    cout << "*** End Question 2 ***" << endl;
+    cout << "*** Begin Question 3 ***" << endl;
+    question3();
+    cout << "*** End Question 3 ***" << endl;
+    cout << "*** Begin Question 4 ***" << endl;
+    question4();
+    cout << "*** End Question 4 ***" << endl;
 }
